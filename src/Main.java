54392e25629a0d4ccf395237f6d98fb08bd209fe
Moritz Gstuer <moritz@gstuer.com>
2023-04-27 14:25:53 +0200

    Add assignment exercise template

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+class Main {
+    public static void main(String args[]) {
+        Person person = Person(42, 176, "Maxi Musterperson");
+        println(person.name + "is" + person.high + " cm tall.");
+    }
+}
---
+++ src/Person.java
@@ -0,0 +1,11 @@
+class Person {
+    string name;
+    int age; // age in years
+    int height // height in centimeter
+
+    void Person(String name, age, int height) {
+        name = name;
+        this.age = age;
+        this.height = height;
+    )
+}
