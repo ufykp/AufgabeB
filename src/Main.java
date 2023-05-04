cecc1c6dc921514ace05e413f681940bbbd74811
ufykp <ufykp@student.kit.edu>
2023-05-04 19:34:42 +0200

    submit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 class Main {
     public static void main(String args[]) {
-        Person person = Person(42, 176, "Maxi Musterperson");
-        println(person.name + "is" + person.high + " cm tall.");
+
+        Person person = new Person("Maxi Musterperson", 42 ,176);//the order of the 2 munbers ist reversed
+        //If people want to create an object,  need to implement it through the new keyword, which is missing here
+
+        System.out.println(person.name + "is" + person.height + " cm tall.");//"height" is misspelled "high"
+        //Because println() is a method, a class 'System' is required,
+        // and out represents the output attribute. so:'System.out.println()' indicates outputting one sentence
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,11 +1,25 @@
+/**
+ *String is a class, not a basic data type, so it should start with an uppercase letter
+ *After declaring a variable as int, it needs to be added ';'
+ * Void is a return type that indicates that the method will not return any values.
+ * und there is no need for the void keyword here
+ *A class needs to be modified with "accessible modifier" to indicate whether it can be accessed by other classes.
+ * if we want other classes to use this Person class,
+ * then we need to declare this class as public so that other classes can reference it.
+ */
 class Person {
-    string name;
-    int age; // age in years
-    int height // height in centimeter
+    String name;
+    int age;
+    int height;
 
-    void Person(String name, age, int height) {
-        name = name;
+    public Person(String name ,int age ,int height){
+        //A variable must have its type defined before use,
+        //which must be a basic type or a reference type.Because age is an integer, we use int to modify age
+
+        this.name = name;//If a local variable with the same name as a member variable is used,
+        // then the 'this' keyword needs to be used to distinguish between member variables and local variables
+
         this.age = age;
         this.height = height;
-    )
+    }//The curly braces should appear in pairs
 }
