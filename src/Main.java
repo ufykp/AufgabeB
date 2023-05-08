3c0daf7062ea994bc775b1483fe22b71ce1d5343
ufykp <ufykp@student.kit.edu>
2023-05-08 14:56:15 +0200

    submit
    submit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 class Main {
     public static void main(String args[]) {
 
-        Person person = new Person("Maxi Musterperson", 42 ,176);//the order of the 2 munbers ist reversed
+        Person person = new Person("Maxi Musterperson", 42 ,176);//the order of the 2 parameter ist reversed
         //If people want to create an object,  need to implement it through the new keyword, which is missing here
 
         System.out.println(person.name + "is" + person.height + " cm tall.");//"height" is misspelled "high"
